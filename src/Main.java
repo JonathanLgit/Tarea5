2470b9f0adcd208a8bb6f54df73acfba69d0b057
Jonathan <jolm.020@outlook.com>
2024-02-16 18:11:37 -0600

    T5

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        operations(8,25);
+
+    }
+
+     static void operations (double value1 , double value2 ){
+        double sum = value1 + value2;
+        double subtract = value1 - value2;
+        double division = value1 / value2;
+        double module = value1 % value2;
+    }
+}
